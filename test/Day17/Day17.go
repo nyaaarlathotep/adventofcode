2857504ee4cee3e388cca20c7b81a9bef2b178e1
yuegenhua <nyaaarlathotep@163.com>
2021-12-17 17:08:30 +0800

    feat reconstruct
    unfinished, why

--- test/Day17/Day17.go
+++ test/Day17/Day17.go
@@ -8,19 +8,44 @@ func Seventeen(input string) int {
 	area = append(area, xArea)
 	area = append(area, yArea)
 	areaP := &area
+
+	//maxHeight:=	partOneFlyTest(xArea[1]/2+1,-yArea[0],areaP)
+	hitCount := partTwoFlyTest(xArea[1]/2+1, yArea[0], -yArea[0], areaP)
+	return hitCount
+}
+
+func partOneFlyTest(vXMax int, vYMax int, areaP *[][]int) int {
 	maxHeight := -1
 
-	for vX := 0; vX <= xArea[1]/2+1; vX++ {
-		for vY := 0; vY <= -yArea[0]; vY++ {
+	for vX := 0; vX <= vXMax; vX++ {
+		for vY := 0; vY <= vYMax; vY++ {
 			probe := NewProbe(vX, vY)
 			for !probe.Out(areaP) {
 				probe.fly(areaP)
 				if probe.Hit && probe.MaxHeight > maxHeight {
 					maxHeight = probe.MaxHeight
+					break
 				}
 			}
 		}
 	}
-
 	return maxHeight
+}
+
+func partTwoFlyTest(vXMax int, vYMin int, vYMax int, areaP *[][]int) int {
+	hitCount := 0
+
+	for vX := 0; vX <= vXMax; vX++ {
+		for vY := vYMin; vY <= vYMax; vY++ {
+			probe := NewProbe(vX, vY)
+			for !probe.Out(areaP) {
+				probe.fly(areaP)
+				if probe.Hit {
+					hitCount++
+					break
+				}
+			}
+		}
+	}
+	return hitCount
 }
